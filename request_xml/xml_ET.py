52396c4d413bc3706fb31f68c5b8eb518b4c4138
Ginkgo <ginkgo.dia@gmail.com>
2018-01-02 23:05:39 +0800

    add more xml

--- request_xml/xml_ET.py
+++ request_xml/xml_ET.py
@@ -12,4 +12,30 @@ except ImportError:
 ret = ET.ElementTree(file="doc.xml")
 # print(ret)
 root = ret.getroot()
-print(root.tag, root.attrib)
+# print(root.tag, root.attrib)
+"""
+for items in root:
+    print(items.tag, items.attrib)
+print(root[0].tag)
+print(root[0].attrib)
+print(root[0].text)
+print("=========")
+print(root[0][0].tag)
+print(root[0][0].attrib)
+print(root[0][0].text)
+print(root[0][1].tag)
+
+for item in root.iter(tag="book"):
+    print(item.tag, item.attrib)
+
+for items in root.iter(tag="title"):
+    print(items.tag, items.attrib)
+"""
+for item in root.iterfind("book/title"):
+    print(item.tag, item.text)
+
+for items in root.findall("book"):
+    title = items.find("title").text
+    price = items.find("price").text
+    lang = items.find("title").attrib.get("lang")
+    print(title, price, lang)
